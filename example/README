cb4e5347c3c9f97674e23e1ab4437c35500c5a9f
wbroek <woutervdbroek@gmail.com>
2010-08-17 10:23:48 +0200

    Initial GIT import of ipaDistribution

---
+++ example/README
@@ -0,0 +1,9 @@
+# IPA Distribution class example
+
+## What is this example
+
+This example PHP script finds all the IPA files in the current directory and throws them to the IPA Distribution class, which creates the Wireless Manifest for iOS Wireless App Distribution. If any Provision Profiles are found in the same directory it will mach them.
+
+With every IPA file it finds it creates an list item with the icon (if available) and name which links to the manifest file so you can install the app on your iOS 4 device. If the Provision Profile is found it will create a list item beneath the app so this can be installed too.
+
+The whole page is styled with the CSS from UiUIKit Framework, this creates the native look it has. 
---
+++ example/example.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+	<title>Example</title>
+	<link rel="stylesheet" href="stylesheets/iphone.css" />
+	<style type="text/css">.smaller { font-size: 80%; } </style>
+	<script type="text/javascript" charset="utf-8">
+		window.onload = function() {
+		  setTimeout(function(){window.scrollTo(0, 1);}, 100);
+		}
+	</script>
+</head>
+<body>
+	
+	<div id="header">
+		<h1>Applications</h1>
+	</div>
+<?
+include('../ipaDistrubution.php');
+foreach (glob("*.ipa") as $filename) {
+$ipa = new ipaDistrubution($filename);
+	if ($ipa->appname!=""&&$ipa->applink!="") {
+?>	
+	<ul>
+		<li><a href="<?=$ipa->applink?>"><?=($ipa->appicon!=""?'<img src="'.$ipa->appicon.'" width="32" class="ico" /> ':'') ?> <?=$ipa->appname?></a></li>
+		<? if ($ipa->provisionprofile!="")  { ?> 
+		<li><a href="<?=$ipa->provisionprofile?>" class="smaller"><img src="http://www.footballbet.net/images/prov.png" height="32" class="ico" /> Provisioning File for <?=$ipa->appname?> </a></li>
+		<? } ?>
+	</ul>
+<?	}
+}
+?>
+	
+<p><strong>Example for ipaDistrubution class</strong><br />this example is made by Wouter van den Broek, CSS from UiUIKit Framework</p>
+	
+</body>
+</html>
+
+
+
+
+
